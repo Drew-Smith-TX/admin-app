44831c877aaade31ed1b6b81dc1aca1367516597
Drew Smith <BJASmith@gmail.com>
2020-05-26 21:24:08 -0500

    working customer

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -11,38 +11,29 @@ import { Observable } from 'rxjs/internal/Observable';
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent implements AfterContentInit, OnChanges, OnInit, AfterViewChecked{
-  // public title: Observable<string>;
-  // other: string;
-  // subscription: Subscription;
-  @ViewChild(AppComponent) root: AppComponent;
+export class NavComponent implements  OnInit{
+
+  // @ViewChild(AppComponent) root: AppComponent;
   monitorTitle: string;
   titleObservable: Observable<string>;
+  
   constructor(public cardService: CardService,
               private headService: HeaderService,
-              public cdr: ChangeDetectorRef) { 
-               
-  }
+              public cdr: ChangeDetectorRef) { }
   ngOnInit(){
    this.titleObservable = this.headService.titleSubject.asObservable();
    this.titleObservable.subscribe((title) =>{
      this.monitorTitle = title;
      console.log('Title set')
      console.log(this.monitorTitle);
-   })
+   });
+   
   }
-  ngAfterViewChecked(): void {
-    
-   }
-   ngAfterViewInit(){
-     
-   }
-   ngOnChanges(){
-    
-   }
-   ngAfterContentInit() {
-     
-   }
+  
+  
+  
+   
+  
   
 
 
